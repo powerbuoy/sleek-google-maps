cf89d53f8742070cc660cba432507b5d255272bf
AL <powerbuoy@gmail.com>
2019-10-28 20:59:42 +0100

    Dont keep generating maps :P

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (typeof gmInit !== 'undefined' && typeof SLEEK_GOOGLE_MAPS_API_KEY !== 'undef
 	gmInit(function () {
 		var observer = new IntersectionObserver(entries => {
 			entries.forEach(entry => {
-				if (entry.isIntersecting) {
+				if (entry.isIntersecting && !entry.target.sleekMap) {
 					entry.target.sleekMap = new SleekMap(entry.target, SleekMap.parseMapEl(entry.target));
 				}
 			});
